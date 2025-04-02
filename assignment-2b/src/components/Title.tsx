61623a5b3d4205794154e0188b1945c7eead4c9c
System Administrator <root@Hudsons-MacBook-Air.local>
2025-04-02 00:14:24 -0400

    added tailwind, and dasiy ui to experament with component libraries. started website asignment

---
+++ assignment-2b/src/components/Title.tsx
@@ -0,0 +1,16 @@
+
+function Title () {
+
+   return (
+        <div className="2xl:bg-base-100/70">
+
+            <div className="text-xl text-accent text-accent">
+                CS3733-D25 Team 97: Pottery and Cello
+            </div>
+
+        </div>
+        )
+
+}
+
+export default Title
